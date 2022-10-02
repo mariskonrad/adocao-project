cd3ecb5f178012e695d0e67bb704771ae14fcce6
Marisbel Konrad <marisbel.konrad@cwi.com.br>
2022-10-02 15:41:54 -0300

    add inserts file

---
+++ api/data/inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO pet (name, age, size, sex, breed, image, available)
+VALUES ('Fefinho', 2, 'MEDIUM', 'M', 'Vira-lata', 'https://images.unsplash.com/photo-1564087040905-670d5e551bac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80', true);
+
+INSERT INTO pet (name, age, size, sex, breed, image, available)
+VALUES ('Espirrinha', 4, 'SMALL', 'F', 'Vira-lata', 'https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', true);
+
+INSERT INTO pet (name, age, size, sex, breed, image, available)
+VALUES ('Sereninho', 6, 'SMALL', 'M', 'Vira-lata', 'https://images.unsplash.com/photo-1514373941175-0a141072bbc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80', true);
+
+INSERT INTO pet (name, age, size, sex, breed, image, available)
+VALUES ('Bili', 4, 'BIG', 'M', 'Vira-lata', 'https://images.unsplash.com/photo-1560525821-d5615ef80c69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', true);
