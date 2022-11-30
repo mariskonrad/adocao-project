9adccc9dbc8d2a5a50b747eda0d98730a2bed8b6
Marisbel Konrad <marisbel.konrad@cwi.com.br>
2022-11-30 19:48:20 -0300

    contact screen style

--- app/src/ui/screens/contact/contact.screen.jsx
+++ app/src/ui/screens/contact/contact.screen.jsx
@@ -1,3 +1,31 @@
+import { Header } from '../../components'
+import EMAIL from '../../../images/email.png'
+import PHONE from '../../../images/whats.png'
+import './contact.style.css'
+
 export function ContactScreen() {
-  return 'Contact screen'
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <div className='contact-container'>
+          <div className='contact-box'>
+            <p className='title'>Entre em contato conosco</p>
+            <div className='contact-info'>
+              <div>
+                <img src={EMAIL} alt='Ícone e-mail' className='icons' />
+              </div>
+              <div className='text-tertiary'>associacao@gmail.com</div>
+            </div>
+            <div className='contact-info'>
+              <div>
+                <img src={PHONE} alt='Ícone telefone' className='icons' />
+              </div>
+              <div className='text-tertiary'>(51) 99999999</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
