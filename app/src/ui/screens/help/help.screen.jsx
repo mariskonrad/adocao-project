07e7c28fd1bc7cf004062b018f05b53b6afd2fc6
Marisbel Konrad <marisbel.konrad@cwi.com.br>
2022-11-30 19:19:08 -0300

    create help screen

--- app/src/ui/screens/help/help.screen.jsx
+++ app/src/ui/screens/help/help.screen.jsx
@@ -1,3 +1,34 @@
+import { Header } from '../../components/header/header.component'
+import './help.style.css'
+
 export function HelpScreen() {
-  return 'Help screen'
+  return (
+    <>
+      <Header />
+      <div className='help-container container'>
+        <div className='title'>Como ajudar</div>
+        <div className='help-content'>
+          <div className='help-content-text'>
+            <p className='text-primary'>Voluntariado</p>
+            <p className='text-tertiary'>
+              Os voluntários ajudam nas tarefas do canil, nos eventos, no transporte dos animais, doações, nos
+              banhos e nos passeios dos nossos animais.
+            </p>
+          </div>
+          <div className='help-content-text'>
+            <p className='text-primary'>Parcerias</p>
+            <p className='text-tertiary'>
+              Para empresas que queiram formar parcerias para atendimento veterinário, castração, venda de
+              produtos com porcentagem de doação para a Associação, doação de produtos, etc.
+            </p>
+          </div>
+          <div className='help-content-text'>
+            <p className='text-primary'>Doação em dinheiro</p>
+            <p className='text-tertiary'>Toda e qualquer doação é bem vinda e muito importante!</p>
+            <p className='text-tertiary'>Chave PIX para doação: 99999999</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
